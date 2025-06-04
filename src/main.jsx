086bb1443f546bbb1a7df399891c679805bbca9f
Fiorezarn <fioreza1902@gmail.com>
2025-06-04 10:30:59 +0700

    remove analytic

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import './index.css';
 import Router from './Router';
 import { Provider } from 'react-redux';
 import { store } from './redux-toolkit/store';
-import { Analytics } from '@vercel/analytics/next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
   <Provider store={store}>
    <Router />
-   <Analytics />
   </Provider>
  </React.StrictMode>
 );
