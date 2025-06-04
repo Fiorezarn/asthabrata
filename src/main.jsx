ec6fd23f51db057b8eb784a05e19f01a1415b87c
Fiorezarn <fioreza1902@gmail.com>
2025-06-04 10:25:08 +0700

    add vercel analytic

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Router from "./Router";
-import { Provider } from "react-redux";
-import { store } from "./redux-toolkit/store"; // Pastikan path ini sesuai dengan lokasi store Anda
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Router from './Router';
+import { Provider } from 'react-redux';
+import { store } from './redux-toolkit/store';
+import { Analytics } from '@vercel/analytics/next';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+  <Provider store={store}>
+   <Router />
+   <Analytics />
+  </Provider>
+ </React.StrictMode>
 );
